5c59db6bdeecf14e01f8123049a11ebfc204ae58
Anatolios Laskaris <github_me@nahsi.dev>
2023-11-21 20:28:58 +0200

    chore: Fix ipfs (#82)
    Fix ipfs

--- deployment/ipfs/10-configure.sh
+++ deployment/ipfs/10-configure.sh
@@ -1,5 +1,11 @@
 #! /usr/bin/env sh
 
+if ! (( (ls -1 "$IPFS_PATH" | wc -l) )); then
+	ipfs init
+fi
+
+ipfs bootstrap rm --all
+
 ipfs config Addresses.API "/ip4/0.0.0.0/tcp/5020"
 
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
